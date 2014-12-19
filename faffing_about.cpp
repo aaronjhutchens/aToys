0b48dd75434f0a606dbbd959fe38fe08e6f08dbb
Aaron H <AHutchens@AHUTCHENS-NBK1.ip-soft.net>
2014-12-19 12:03:58 -0600

    added death stuff

--- faffing_about.cpp
+++ faffing_about.cpp
@@ -4,89 +4,124 @@
 #include <time.h>
 using namespace std;
 
+enum status { KO, Poisoned, None /*more to come for sure.*/ };
 
 class player
 {
 private:
 	int strength, hitPoints;
 	string charName;
+	status playerStatus;
 
 public:
 	~player() {};
 	player(string initName){ charName = initName; };
-	player(string initName, int initStr){
+	player(string initName, int initStr)
+	{
 		charName = initName;
 		strength = initStr;
 	}
-	player(string initName, int initStr, int initHitPoints){
+	player(string initName, int initStr, int initHitPoints)
+	{
 		charName = initName;
 		strength = initStr;
 		hitPoints = initHitPoints;
 	}
+	status getStatus()
+	{
+		return playerStatus;
+	}
+	void setStatus(status newStatus)
+	{
+		playerStatus = newStatus;
+	}
 
-	void setName(string newName){
+	void setName(string newName)
+	{
 		charName = newName;
 	}
-	string getName(){
+	string getName()
+	{
 		return charName;
 	}
-	int getStr(){
+	int getStr()
+	{
 		return strength;
 	}
-	void setStr(int newStr){
+	void setStr(int newStr)
+	{
 		strength = newStr;
 	}
-	int getHP(){
+	int getHP()
+	{
 		return hitPoints;
 	}
-	void getHP(int pretty){
+	void getHP(bool pretty)
+	{
 		cout << getName() << " has " << getHP() << " hit points\n";
 	}
-	void setHitPoints(int newHP){
+	void setHP(int newHP)
+	{
 		hitPoints = newHP;
 	}
-	void getStats(){
+	void getStats()
+	{
 		cout << "Name:\t" << getName() << endl;
 		cout << "\tHit Points: " << getHP() << endl;
 		cout << "\tStrength: " << getStr() << "\n\n";
+	}
+	void knockOut()
+	{
+		if (getHP() <= 0)
+		{
+			setHP(0);
+
+			cout << getName() << " has died\n\n";
+		}
 	}
 
 };
 
 class duel
-	/*
-	* Ideally this will be a (singleton?) mediator class.
-	* Let's see how it goes.
-	*/
 {
+	/*
+	 * Ideally this will be a (singleton?) mediator class.
+	 * Let's see how it goes.
+	 */
+
 private:
-	int getRandom(int multiplier){
+	int getRandom(int multiplier)
+	{
 		//int var = ;
 		return rand() % multiplier;
 	}
 public:
 	duel(){ srand(time(NULL)); };
 	~duel(){};
-	void attack(player &attacker, player &defender){
+	void attack(player &attacker, player &defender)
+	{
 		//do stuff. Attacky like.
 		int attackVal = getRandom(attacker.getStr());
 		cout << "\nAttacker " << attacker.getName() << " hits " << defender.getName() << " for " << attackVal << " HP\n\n";
-		defender.setHitPoints(defender.getHP() - attackVal);
+		defender.setHP(defender.getHP() - attackVal);
 		attacker.getHP(1);
 		defender.getHP(1);
 	}
-	string battle(player &p1, player &p2){
+	string battle(player &p1, player &p2)
+	{
 		// To the death!
 		string winner;
 
-		while (p1.getHP() > 0 && p2.getHP() > 0){
+		while (p1.getHP() > 0 && p2.getHP() > 0)
+		{
 			attack(p1, p2);
 			attack(p2, p1);
 		}
 		if (p1.getHP() > 0){
 			winner = p1.getName();
 		}
-		else if (p2.getHP() > 0){
+		else if (p2.getHP() > 0)
+		{
 			winner = p2.getName();
 		}
 		return winner;
